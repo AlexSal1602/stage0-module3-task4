920b9b409346de74a2ce85eced405657ebf29080
AlexSal1602 <asalbishvili@hotmail.com>
2023-08-07 17:43:30 +0400

    module3 task4 try3

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a;
-        int b;
+        int a=5;
+        int b=10;
 
-        int formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/2);
+        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/2);
     }
 }
